73a7a1deee036e787ac7d0e3a7b158d5794b6b14
gougouwen <gougouwen@tencent.com>
2025-11-17 20:58:22 +0800

    优化

--- parser/statement_parser.go
+++ parser/statement_parser.go
@@ -1,1 +1,24 @@
 package parser
+
+import (
+	"model_infrax/config"
+	"model_infrax/model"
+)
+
+type StatementParser struct {
+	configger *config.Configger
+}
+
+func NewStatementParser(cfg *config.Configger) (*StatementParser, error) {
+	return &StatementParser{
+		configger: cfg,
+	}, nil
+}
+
+func (p *StatementParser) Parse() (schemas []model.Schema, err error) {
+	return nil, nil
+}
+
+func (p *StatementParser) FilterTables(schemas []model.Schema) (filtered []model.Schema) {
+	return
+}
