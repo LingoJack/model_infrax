f2715d43fe1d702d135a33d904509fd896f8db84
gougouwen <gougouwen@tencent.com>
2025-11-18 16:24:21 +0800

    优化

--- assert/install.sh
+++ assert/install.sh
@@ -1,8 +1,12 @@
 #!/bin/zsh
 echo 'Setting environment...'
 cp ~/.zshrc ~/.zshrc.bak
-grep -v "export PATH=\"/Applications/jen/" ~/.zshrc > ~/.zshrc.tmp
-echo 'export PATH="/Applications/jen/"' >> ~/.zshrc.tmp
+
+grep -v "export PATH=\"/Applications/jen/\"" ~/.zshrc > ~/.zshrc.tmp
+
+echo 'export PATH="/Applications/jen/:$PATH"' >> ~/.zshrc.tmp
+
 mv ~/.zshrc.tmp ~/.zshrc
 source ~/.zshrc
 echo '✅ Environment done (original zshrc file backup at ~/.zshrc.bak)'
+echo '📝 jen directory has been added to PATH while preserving existing PATH entries'
