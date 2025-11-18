18939c05f8e0f92f926d801d6379cc30a4fcef8e
gougouwen <gougouwen@tencent.com>
2025-11-18 16:19:38 +0800

    优化

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@ go build -o jen main.go wire_gen.go
 mkdir -p ~/dev/model_infrax/target/
 mkdir -p ~/dev/model_infrax/target/assert/
 mv jen ~/dev/model_infrax/target/
-cp -r ~/dev/model_infrax/assert/ ~/dev/model_infrax/target/assert/
-mv ~/dev/model_infrax/target/assert/application.yml ~/dev/model_infrax/target/
-mv ~/dev/model_infrax/target/assert/schema.sql ~/dev/model_infrax/target/
-mv ~/dev/model_infrax/target/assert/install.sh ~/dev/model_infrax/target/
+cp -r ~/dev/model_infrax/assert/prompt/ ~/dev/model_infrax/target/assert/
+cp ~/dev/model_infrax/assert/application.yml ~/dev/model_infrax/target/
+cp ~/dev/model_infrax/assert/schema.sql ~/dev/model_infrax/target/
+cp ~/dev/model_infrax/assert/install.sh ~/dev/model_infrax/target/
 mkdir -p ~/dev/model_infrax/target/output/
 mkdir -p ~/dev/model_infrax/pack/
 cd ~/dev/model_infrax/target && zip -r ~/dev/model_infrax/pack/jen.zip .
