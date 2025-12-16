b934493ed4276e4482fa5b39e1b40c66e385ae09
gougouwen <gougouwen@tencent.com>
2025-12-16 20:50:03 +0800

    优化

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e  # 遇到错误时退出
 mkdir -p model_infrax
 cd model_infrax
 
-curl -O "https://github.com/LingoJack/model_infrax/raw/refs/heads/main/release/model_infrax.tar.gz"
+curl -LsSfO "https://github.com/LingoJack/model_infrax/raw/refs/heads/main/release/model_infrax.tar.gz"
 tar -xzf model_infrax.tar.gz
 rm model_infrax.tar.gz
 
