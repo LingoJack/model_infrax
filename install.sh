8be8d272f71e5294ac43059bb8ba90cae355ad53
gougouwen <gougouwen@tencent.com>
2025-12-16 20:14:16 +0800

    优化

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
 #!bin/bash
 echo "installing..."
+curl -LO https://github.com/LingoJack/model_infrax/blob/main/release/model_infrax.tar.gz
+tar -xzf model_infrax.tar.gz -C /Applications/
+rm -rf model_infrax.tar.gz
+echo "install success"
+
+echo "setting env..."
+export PATH="/Applications/model_infrax:$PATH"
+echo "export PATH=\"/Applications/model_infrax:$PATH\"" >> ~/.zshrc
+source ~/.zshrc
+echo "env set success"
+
+echo "jen version: $(jen -v)"
