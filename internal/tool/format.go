72e67a5bdd6e4633f2027ec68bd1fa5a28309e43
gougouwen <gougouwen@tencent.com>
2026-01-16 12:29:34 +0800

    优化

--- internal/tool/format.go
+++ internal/tool/format.go
@@ -2,13 +2,11 @@ package tool
 
 import (
 	"go/format"
-	"log"
 )
 
 func FormatGoCode(code string) string {
 	formatted, err := format.Source([]byte(code))
 	if err != nil {
-		log.Printf("格式化 Go 代码失败: %v", err)
 		return code
 	}
 	return string(formatted)
