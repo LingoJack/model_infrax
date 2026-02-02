447929073b400ec9b8d23b8d7848ed38b0c184f4
gougouwen <gougouwen@tencent.com>
2026-02-02 21:18:14 +0800

    优化

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,20 @@
 go build -o jen cmd/jen/main.go
-mkdir -p ~/dev/model_infrax/target/
-mkdir -p ~/dev/model_infrax/target/assets/
-mv jen ~/dev/model_infrax/target/
-cp -r ~/dev/model_infrax/assets/prompt/ ~/dev/model_infrax/target/assets/
-cp ~/dev/model_infrax/assets/model_infrax.yml ~/dev/model_infrax/target/
-cp ~/dev/model_infrax/assets/schema.sql ~/dev/model_infrax/target/
-cp ~/dev/model_infrax/assets/jcode ~/dev/model_infrax/target/
-mkdir -p ~/dev/model_infrax/target/output/
-mkdir -p ~/dev/model_infrax/release/
+mkdir -p ~/dev_custom/model_infrax/target/
+mkdir -p ~/dev_custom/model_infrax/target/assets/
+mv jen ~/dev_custom/model_infrax/target/
+cp -r ~/dev_custom/model_infrax/assets/prompt/ ~/dev_custom/model_infrax/target/assets/
+cp ~/dev_custom/model_infrax/assets/model_infrax.yml ~/dev_custom/model_infrax/target/
+cp ~/dev_custom/model_infrax/assets/schema.sql ~/dev_custom/model_infrax/target/
+cp ~/dev_custom/model_infrax/assets/jcode ~/dev_custom/model_infrax/target/
+mkdir -p ~/dev_custom/model_infrax/target/output/
+mkdir -p ~/dev_custom/model_infrax/release/
 
 # 创建压缩包，如果文件存在则覆盖
 PACKAGE_NAME="model_infrax.tar.gz"
 PACKAGE_PATH="$HOME/dev/model_infrax/release/$PACKAGE_NAME"
 
 # 切换到目标目录并创建压缩包
-cd ~/dev/model_infrax/target && tar -czf "$PACKAGE_PATH" .
+cd ~/dev_custom/model_infrax/target && tar -czf "$PACKAGE_PATH" .
 
 # 输出打包结果信息
 echo "📦 打包完成: $PACKAGE_NAME"
